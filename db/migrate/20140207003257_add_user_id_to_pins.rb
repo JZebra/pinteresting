986963615ff672092df7a06a32605cf7ab2e3b84
JZebra <ziplocpicker@gmail.com>
2014-02-07 15:07:37 -0800

    Changed pins migration, trying to resolve user_id error

--- db/migrate/20140207003257_add_user_id_to_pins.rb
+++ db/migrate/20140207003257_add_user_id_to_pins.rb
@@ -1,6 +1,15 @@
 class AddUserIdToPins < ActiveRecord::Migration
-  def change
-    add_column :pins, :user_id, :integer
-    add_index :pins, :user_id
-  end
+ # def change
+  #  add_column :pins, :user_id, :integer
+   # add_index :pins, :user_id
+  #end
+#end
+
+	def change
+	  create_table :pins do |t|
+	    t.integer :user_id
+	    t.timestamps
+	  end
+	  add_index :pins, :user_id
+end
 end
